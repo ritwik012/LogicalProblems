ad69e3d91bf5c666aa9236f342e54dc2227e47ea
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-12 18:14:51 +0530

    [Rutwik]Added:FibonanciSeries LogicalProblems Features Implemented

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse a Number \n 5. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
