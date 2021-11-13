5107f452c82488698bd796fd80754be36122fda8
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-14 00:10:04 +0530

    [Rutwik]Added:DayOfWeek LogicalProblems Features Implemented

---
+++ LogicalProblems/DayOfWeek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    public class DayOfWeek
+    {
+        public void Day()
+        {
+            Console.WriteLine("Enter the day : ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the month : ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Day is " + dayOfWeek(day, month, year));
+        }
+        public static int dayOfWeek(int d, int m, int y)
+        {
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + (31 * m0) / 12) % 7;
+            return d0;
+        }
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse a Number \n 5. Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse a Number \n 5. Coupon Numbers \n 6. Simulate Stopwatch Program \n 7. Vending Machine \n 8. Day of Week \n 9. Temperature Conversion \n 10. Monthly Payment \n 11. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -22,10 +22,28 @@ namespace LogicalPrograms
                         number.Perfect();
                         break;
                     case 3:
+                        PrimeNumber num = new PrimeNumber();
+                        num.Prime();
                         break;
                     case 4:
+                        ReverseANumber rev = new ReverseANumber();
+                        rev.Reverse();
                         break;
                     case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        DayOfWeek day = new DayOfWeek();
+                        day.Day();
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
                         flag = false;
                         break;
                 }
